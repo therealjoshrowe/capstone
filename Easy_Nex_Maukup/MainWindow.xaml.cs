1e9faf90e7b67fd70a35ccf621a05b1727a5d8bd
Joshua Rowe <jkrowe@ius.edu>
2016-01-24 17:21:02 -0500

    inital commit. Just adding files to github

---
+++ Easy_Nex_Maukup/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Easy_Nex_Maukup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NexusWriter w;
+        String s;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Taxa_Data_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            s = Taxa_Data.Text.ToString();
+
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> l = new List<String>();
+            String [] array = s.Split(new char[] { ' ' } );
+            for (var i = 0; i < array.Length; ++i)
+            {
+                l.Add(array[i]);
+            }
+            w = new NexusWriter(l);
+            w.WriteToFile();
+        }
+    }
+}
---
+++ Easy_Nex_Maukup/NexusWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Nex_Maukup
+{
+    class NexusWriter
+    {
+        private List<String> taxa;
+        public NexusWriter(List<String> taxa)
+        {
+            this.taxa = new List<String>(taxa);
+        }
+
+        public void WriteToFile()
+        {
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\capstone\output.nex"))
+            {
+                file.WriteLine("#NEXUS");
+                file.WriteLine("BEGIN TAXA;");
+                file.WriteLine("Dimensions NTax=" + taxa.Count);
+                file.Write("TaxLabels ");
+
+                foreach (string taxon in taxa)
+                {
+                    file.Write(taxon + " ");
+                }
+                file.WriteLine();
+
+                file.WriteLine("END;");
+            }
+        }
+    }
+}
